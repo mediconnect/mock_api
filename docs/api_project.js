19fb2a7f78da7dd419c063fba32fe39483f244e5
Arnoldosmium <eaglemewpsy@gmail.com>
2018-08-13 22:33:30 -0400

    add api docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-14T02:33:09.041Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
