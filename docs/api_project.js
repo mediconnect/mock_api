6b8f8a5c1fff850644e800372504afdab9ce4e0f
gabrielwry <me@gabrielwry.com>
2018-10-29 22:22:15 -0400

    doc and data

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-14T02:33:09.041Z",
+    "time": "2018-10-30T02:13:07.296Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
